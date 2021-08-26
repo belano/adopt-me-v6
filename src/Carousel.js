e1ac69be69f0acb8ac8c5cc402a293fec45032d2
Diego Lisbona <diego.lisbona@sky.uk>
2021-08-26 19:39:55 +0100

    Interactive class component

--- src/Carousel.js
+++ src/Carousel.js
@@ -9,6 +9,14 @@ class Carousel extends Component {
     images: ["http://pets-images.dev-apis.com/pets/none.jpg"],
   };
 
+  // Arrow Functions lexically bind their context so `this` actually refers to the originating context
+  // ie. it uses `this` from the code that contains the Arrow Function
+  handleIndexClick = (event) => {
+    this.setState({
+      active: +event.target.dataset.index,
+    });
+  };
+
   render() {
     const { active } = this.state;
     const { images } = this.props;
@@ -17,9 +25,12 @@ class Carousel extends Component {
         <img src={images[active]} alt="animal" />
         <div className="carousel-smaller">
           {images.map((photo, index) => (
+            // eslint-disable-next-line
             <img
               key={photo}
               src={photo}
+              data-index={index}
+              onClick={this.handleIndexClick}
               className={index === active ? "active" : ""}
               alt="animal thumbnail"
             />
